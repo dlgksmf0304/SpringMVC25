c0cfadce3dce710962a8adb7a7c754ee4b08ce3c
220-15\dlgksmf0304 <dlgksmf0304@gmail.com>
2025-07-11 18:04:15 +0900

    프론트연결 완료

--- src/main/java/org/mbc/service/BoardService.java
+++ src/main/java/org/mbc/service/BoardService.java
@@ -3,19 +3,14 @@ package org.mbc.service;
 import java.util.List;
 
 import org.mbc.domain.BoardVO;
-import org.springframework.stereotype.Service;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-
 
 public interface BoardService {
-	// 인터페이스는 단독으로 사용할 수 없는 객체다.
-	// 여기서 선언한 메서드는 무조건 추상메서드로 사용된다.(실행문이 없다. -> ;)
+	// 인터페이스는 단독으로 사용할 수 없는 객체임
+	// 여기서 선언한 메서드는 무조건 추상메서드로 사용된다.( 실행문없이 끝난다. -> ; )
 	// 인터페이스를 사용하려면 구현 클래스를 만들고 실행문을 넣게 된다.
 	// 인터페이스를 사용하는 이유는 조장이 메서드명을 강제로 셋팅하기 위함이다.
 	
-	public void register(BoardVO board); // 조장이 만든 메서드명(시그니처)
+	public void register(BoardVO board); // 조장이 만든 메서드명(시그니쳐)
 	// 게시판 등록용 메서드
 	
 	public BoardVO get(Long bno);
@@ -28,8 +23,8 @@ public interface BoardService {
 	// 게시글 삭제하기 (번호가 들어가서 삭제 됐는지 t/f)
 	
 	public List<BoardVO> getList();
-	// 이 메서드가 호출되면 리스트타입으로 BoardVO객체들이 넘어온다.
-	// List : 동적리스트 / <> 제네릭 : 강제로 객체 타입을 정
+	// 이 메서드가 호출되면 리스트타입으로 BoardVO객체 들이 넘어온다.
+	// List : 동적리스트 / < > 제네릭 : 강제로 객체 타입을 정함.
 	
 	
 }
--- src/test/java/org/mbc/mapper/BoardMapperTests.java
+++ src/test/java/org/mbc/mapper/BoardMapperTests.java
@@ -14,56 +14,61 @@ import lombok.extern.log4j.Log4j2;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j2
 public class BoardMapperTests {
-	// board에서 사용하는 데이터베이스 테스트용
+	// board에서 사용하는 데이터베이스 테스트용!!!
+	
 	
 	@Setter(onMethod_ = @Autowired)
-	private BoardMapper mapper; // 인터페이스로 만든 객체를 세터로 연결
+	private BoardMapper mapper;  // 인터페이스로 만든 객체를 세터로 연결
 	
-	@Test //import org.junit.Test; (메서드 단위로 테스트)
+	@Test // import org.junit.Test; (메서드 단위로 테스트)
 	public void testGetList() {
 		
-		mapper.getList().forEach(board -> log.info(board));
-		// 인터페이스.메서드.for문   결과객체
-		//                            -> 람다식(인터페이스용)
-		//                               log.info 콘솔에 출력(board.toString)
-		
+		mapper.getList().forEach(board -> log.info(board) );
+		// 인터페이스.메서드.for문   결과객체 
+		//                             -> 람다식(인터페이스용)
+		//                                log.info 콘솔에 출력(board.toString)
 	}
-	@Test //import org.junit.Test; (메서드 단위로 테스트)
+	
+	
+	@Test // import org.junit.Test; (메서드 단위로 테스트)
 	public void testGetListXML() {
 		
-		mapper.getList2().forEach(board -> log.info(board));
-		// 인터페이스.메서드.for문   결과객체
-		//                            -> 람다식(인터페이스용)
-		//                               log.info 콘솔에 출력(board.toString)
-		
+		mapper.getList2().forEach(board -> log.info(board) );
+		// 인터페이스.메서드.for문   결과객체 
+		//                             -> 람다식(인터페이스용)
+		//                                log.info 콘솔에 출력(board.toString)
 	}
+	
+	
 	
 	@Test
 	public void testInsert() {
 		
-		BoardVO board = new BoardVO();
+		BoardVO board = new BoardVO(); 
 		board.setTitle("제발 오류없어라");
-		board.setContent("이번엔 누구냐!!");
+		board.setContent("이번엔 누구냐!!!");
 		board.setWriter("김기원");
 		
 		mapper.insert(board);
 		
 		log.info(board);
+		
 	}
+	
+	
 	
 	@Test
 	public void testInsertSelectKey() {
 		
 		BoardVO board = new BoardVO();
-		board.setTitle("이번엔 제발 번호 나와라");
-		board.setContent("내 게시물의 번호는 ?");
+		board.setTitle("이번엔 제발!!!! 번호나와롸");
+		board.setContent("내 게시물에 번호는? ");
 		board.setWriter("김기원");
 		
 		mapper.insertSelectKey(board);
 		
 		log.info(board);
-		log.info("내가 만든 게시물의 번호는 : " +  + board.getBno());
-		
+		log.info("내가만든 게시물의 번호는 : " + board.getBno());
 		
 	}
 	
@@ -76,14 +81,15 @@ public class BoardMapperTests {
 		
 	}
 	
+	
 	@Test
 	public void testDelete() {
 		
 		int count = mapper.delete(3L);
-		
 		log.info("삭제된 갯수 출력 : " + count + "건");
-		
 	}
+	
+	
 	
 	@Test
 	public void testUpdate() {
@@ -96,9 +102,11 @@ public class BoardMapperTests {
 		
 		int count = mapper.update(board);
 		log.info("수정된 갯수 : " + count + "건");
-		log.info("수정된 객체 출력 : " + board);
+		log.info("수정된 객체 출력 :" + board);
+		
+		
+		
+		
 		
 	}
-	
-
 }
